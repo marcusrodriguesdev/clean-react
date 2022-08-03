881c92b95437ed7a81c023a4cf2a19d58dc569fb
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-08-03 20:32:19 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
@@ -7,13 +7,30 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
-      '@': path.join(__dirname, 'src'),
-    },
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: './public',
@@ -22,9 +39,9 @@ module.exports = {
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM',
+    'react-dom': 'ReactDOM'
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ]
 }

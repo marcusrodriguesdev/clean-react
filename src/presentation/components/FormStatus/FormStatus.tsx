e891866f2bf298d32ea97f2ef2429fd5f8f60d18
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-08-19 19:58:46 -0300

    feat: ensure Login start with inputs with required state

--- src/presentation/components/FormStatus/FormStatus.tsx
+++ src/presentation/components/FormStatus/FormStatus.tsx
@@ -4,11 +4,12 @@ import Styles from './FormStatus.styles.scss'
 import Context from '@/presentation/context/form/FormContext'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state, errorState } = useContext(Context)
+
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
-      { isLoading && <Spinner className={Styles.spinner} /> }
-      { errorMessage && <span className={Styles.error}>Erro</span>}
+      { state.isLoading && <Spinner className={Styles.spinner} /> }
+      { errorState.main && <span className={Styles.error}>Erro</span>}
     </div>
   )
 }
--- src/presentation/components/Input/Input.tsx
+++ src/presentation/components/Input/Input.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './Input.styles.scss'
+import Context from '@/presentation/context/form/FormContext'
 
 type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<InputProps> = (props: InputProps) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
+
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+
+  const getStatus = (): string => {
+    return '🔴'
+  }
+
+  const getTitle = (): string => {
+    return error
   }
 
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly onFocus={enableInput} />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }

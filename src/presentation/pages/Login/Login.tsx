10575330e83b1009736ac5515b2207eab6086fed
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-08-22 15:36:49 -0300

    feat: ensure Login call validation with correct email

--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Styles from './Login.styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/context/form/FormContext'
+import { Validation } from '@/protocols/validation'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
+type Props = {
+  validation?: Validation
 }
 
-const Login: React.FC = () => {
-  const [state] = useState({
-    isLoading: false
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
+    isLoading: false,
+    email: '',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    mainError: ''
   })
-  const [errorState] = useState({
-    email: 'Campo obrigatório',
-    password: 'Campo obrigatório',
-    main: ''
-  })
+  useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={{ state, errorState }}>
+      <Context.Provider value={{ state, setState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
